1208005bf69c4bb85e37b9d209223d5a75106e18
David masterson <david73180@gmail.com>
2018-08-26 17:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n) {
 function dec(n) {
   return n -= 1
 }
-function makeInt(n, o) {
-  return parseInt(n, o)
+function makeInt(n) {
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
   return parseFloat(n)
